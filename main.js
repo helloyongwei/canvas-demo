98da45ba16d7eb8aa8063080dec2c2343ab25e9a
Helloyong <2628578405@qq.com>
2018-04-09 20:29:56 +0800

    canvas-demo

---
+++ main.js
@@ -0,0 +1,89 @@
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+
+autoSetCanvasSize(canvas)
+
+listenToMouse(canvas)
+
+
+var eraserEnabled = false
+eraser.onclick = function() {
+  eraserEnabled =true
+  actions.className = 'actions x'
+  
+}
+brush.onclick = function(){
+  eraserEnabled = false
+  actions.className = 'actions'
+}
+
+
+function autoSetCanvasSize(canvas) {
+  setCanvasSize()
+
+  window.onresize = function() {
+    setCanvasSize()
+  }
+
+  function setCanvasSize() {
+    var pageWidth = document.documentElement.clientWidth
+    var pageHeight = document.documentElement.clientHeight
+
+    canvas.width = pageWidth
+    canvas.height = pageHeight
+  }
+}
+
+function drawLine(x1, y1, x2, y2) {
+  context.beginPath();
+  context.strokeStyle = 'black'
+  context.moveTo(x1, y1); // 起点
+  context.lineWidth = 5
+  context.lineTo(x2, y2); // 终点
+  context.stroke()
+  context.closePath()
+}
+
+function listenToMouse(canvas) {
+
+
+  var using = false
+  var lastPoint = {
+    x: undefined,
+    y: undefined
+  }
+  canvas.onmousedown = function(event) {
+    var x = event.clientX
+    var y = event.clientY
+    using = true
+    if (eraserEnabled) {
+      context.clearRect(x - 5, y - 5, 10, 10)
+    } else {
+      lastPoint = {
+        "x": x,
+        "y": y
+      }
+    }
+  }
+  canvas.onmousemove = function(event) {
+    var x = event.clientX
+    var y = event.clientY
+
+    if (!using) {return}
+
+    if (eraserEnabled) {
+      context.clearRect(x - 5, y - 5, 10, 10)
+    } else {
+      var newPoint = {
+        "x": x,
+        "y": y
+      }
+      drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+      lastPoint = newPoint
+    }
+
+  }
+  canvas.onmouseup = function(event) {
+    using = false
+  }
+}
